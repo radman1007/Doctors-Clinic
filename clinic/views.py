002fc6399734255d14fc68021f7596af9db2361a
radman1007 <radman.cod110@gmail.com>
2024-07-06 22:42:20 +0330

    debug

--- clinic/views.py
+++ clinic/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'index.html',)
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
