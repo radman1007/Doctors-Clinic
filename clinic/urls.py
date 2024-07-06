301a749bbcda52e8038f927f36383d88428909fb
radman1007 <radman.cod110@gmail.com>
2024-07-06 22:36:42 +0330

    add urls

--- clinic/urls.py
+++ clinic/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+        path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('service/', views.service, name='service'),
+    path('feature/', views.feature, name='feature'),
+    path('team/', views.team, name='team'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('contact/', views.contact, name='contact'),
 ]
