a37d6b0b384bf856b4625e47745c01616dc7ac61
radman1007 <radman.cod110@gmail.com>
2024-07-06 23:19:44 +0330

    add forms

--- clinic/forms.py
+++ clinic/forms.py
@@ -5,3 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         fields = '__all__'
+        
+        
+class TestimonialForm(forms.ModelForm):
+    class Meta:
+        model = models.Testimonial
+        fields = '__all__'
