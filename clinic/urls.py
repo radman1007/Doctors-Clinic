9ff709d106468e85e8e40531979e2ee7b279ced9
radman1007 <radman.cod110@gmail.com>
2024-07-06 22:36:57 +0330

    debug path

--- clinic/urls.py
+++ clinic/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('service/', views.service, name='service'),
     path('feature/', views.feature, name='feature'),
