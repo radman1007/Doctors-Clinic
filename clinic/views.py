eeaf24fd97534828d2ce64834f571dca96a6e271
radman1007 <radman.cod110@gmail.com>
2024-07-06 22:39:06 +0330

    debug

--- clinic/views.py
+++ clinic/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
-from .models import Testimonial, Team
 
 def index(request):
     teams = Team.objects.filter()
