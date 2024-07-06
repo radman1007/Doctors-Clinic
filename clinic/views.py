113d37df99497d05af53b739aa66823b632ce9af
radman1007 <radman.cod110@gmail.com>
2024-07-06 22:38:23 +0330

    add views

--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import ContactForm, TestimonialForm, TeamForm
+from .models import Testimonial, Team
+
+def index(request):
+    teams = Team.objects.filter()
+    forms = Testimonial.objects.filter()
+    context = {
+        'forms' : forms,
+        'teams' : teams,
+    }
+    return render(request, 'index.html', context)
+
+def about(request):
+    teams = Team.objects.filter()
+    context = {
+        'teams' : teams
+    }
+    return render(request, 'about.html', context)
+
+def service(request):
+    forms = Testimonial.objects.filter()
+    context = {
+        'forms' : forms
+    }
+    return render(request, 'service.html', context)
+    
+def feature(request):
+    return render(request, 'feature.html')
+
+def team(request):
+    teams = Team.objects.filter()
+    context = {
+        'teams' : teams
+    }
+    return render(request, 'team.html', context)
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+def testimonial(request):
+    forms = Testimonial.objects.filter()
+    context = {
+        'forms' : forms
+    }
+    return render(request, 'testimonial.html', context)
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact')
+    form = ContactForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'contact.html', context)
