dc15fba9d6dc169e50891c67bdcdec27c192eeae
radman1007 <radman.cod110@gmail.com>
2024-07-07 16:08:34 +0330

    add team model

--- clinic/models.py
+++ clinic/models.py
@@ -25,3 +25,14 @@ class Testimonial(models.Model):
     def __str__(self):
         return self.name
     
+    
+class Team(models.Model):
+    image = models.ImageField()
+    name = models.CharField(max_length=255)
+    department = models.CharField(max_length=255)
+    instagram = models.CharField(max_length=255, null=True)
+    twitter = models.CharField(max_length=255, null=True)
+    facebook = models.CharField(max_length=255, null=True)
+    
+    def __str__(self):
+        return self.name
